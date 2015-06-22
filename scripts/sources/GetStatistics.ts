780f5a39b05e82529b9a152796699c47e1b1efda
Christian Brel <ch.brel@gmail.com>
2015-06-22 11:52:05 +0200

    refs SERVICES-49 : Updated GetStatistics source.

--- scripts/sources/GetStatistics.ts
+++ scripts/sources/GetStatistics.ts
@@ -10,15 +10,33 @@
 /// <reference path="../VigiglobeNamespaceManager.ts" />
 
 var uuid : any = require('node-uuid');
+var moment : any = require('moment');
 
+/**
+ * Represents the Vigiglobe's GetStatistics Source.
+ *
+ * @class GetStatistics
+ * @extends SourceItf
+ */
 class GetStatistics extends SourceItf {
 
 
+	/**
+	 * Constructor.
+	 *
+	 * @param {JSON Object} params - Source's Params.
+	 * @param {VigiglobeNamespaceManager} vigiglobeNamespaceManager - SourceNamespaceManager attached to Source.
+	 */
 	constructor(params : any, vigiglobeNamespaceManager : VigiglobeNamespaceManager) {
 		super(params, vigiglobeNamespaceManager);
 	}
 
-	public run() {
+	/**
+	 * Run the source.
+	 *
+	 * @method run
+	 */
+	run() {
 		var self = this;
 
 		var fail = function(error) {
@@ -29,6 +47,22 @@ class GetStatistics extends SourceItf {
 			Logger.debug(result);
 		};
 
-		
+		var today = moment();
+		today.milliseconds(0);
+		today.seconds(0);
+		today.minutes(0);
+		today.hours(0);
+
+		var timeFrom = encodeURIComponent(today.format());
+
+		today.seconds(59);
+		today.minutes(59);
+		today.hours(23);
+
+		var timeTo = encodeURIComponent(today.format());
+
+		var projectId = self.getParams().VigiglobeProjectId;
+
+		RestClient.get("http://api.vigiglo.be/api/statistics/v1/volume?language=%5B%22fr%22%5D&granularity=day&timeFrom=" + timeFrom + "&count=messages&project_id=" + projectId + "&timeTo=" + timeTo, success, fail);
 	}
 }
