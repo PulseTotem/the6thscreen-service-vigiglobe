0d8b6142a3279481d2539e01b4f0c9bd1eda3253
Christian Brel <ch.brel@gmail.com>
2015-06-26 18:39:05 +0200

    refs SERVICES-50 :  Added GetWordCloud source for Vigiglobe Service.

--- scripts/VigiglobeNamespaceManager.ts
+++ scripts/VigiglobeNamespaceManager.ts
@@ -5,6 +5,7 @@
 /// <reference path="../t6s-core/core-backend/scripts/server/SourceNamespaceManager.ts" />
 
 /// <reference path="./sources/GetStatistics.ts" />
+/// <reference path="./sources/GetWordCloud.ts" />
 
 class VigiglobeNamespaceManager extends SourceNamespaceManager {
 
@@ -17,5 +18,6 @@ class VigiglobeNamespaceManager extends SourceNamespaceManager {
     constructor(socket : any) {
         super(socket);
 	    this.addListenerToSocket('GetStatistics', function(params : any, self : VigiglobeNamespaceManager) { (new GetStatistics(params, self)) });
+		this.addListenerToSocket('GetWordCloud', function(params : any, self : VigiglobeNamespaceManager) { (new GetWordCloud(params, self)) });
     }
 }
