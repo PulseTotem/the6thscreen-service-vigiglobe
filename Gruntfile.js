e5ee51c31752c3d032cb9a50c659cfbbf3453918
Christian Brel <ch.brel@gmail.com>
2015-08-14 15:59:56 +0200

    refs SERVICES-96 : Updated build task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -91,6 +91,9 @@ module.exports = function (grunt) {
             buildPackageReinit: {
                 files: 	[{'package.json': 'package-bak.json'}]
             },
+            keepPackage: {
+              files: 	[{'build/package.json': 'package.json'}]
+            },
             heroku: {
               files: 	[{expand: true, cwd: 'build', src: ['**'], dest: 'heroku'}]
             },
@@ -225,7 +228,7 @@ module.exports = function (grunt) {
     grunt.registerTask('build', function () {
         grunt.task.run(['clean:package', 'clean:build']);
 
-        grunt.task.run(['env:build','update_json:packageBuild', 'copy:buildPackageBak', 'copy:buildPackageReplace', 'npm-install', 'copy:buildPackageReinit', 'typescript:build', 'clean:package']);
+        grunt.task.run(['env:build','update_json:packageBuild', 'copy:buildPackageBak', 'copy:buildPackageReplace', 'npm-install', 'copy:keepPackage', 'copy:buildPackageReinit', 'typescript:build', 'clean:package']);
     });
 
     grunt.registerTask('heroku', function () {
